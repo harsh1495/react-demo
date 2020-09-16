4246b0e1b2d6027bd23b49e49ea6c218a17dd00e
Harsh Mehta <hsmehta1411@gmail.com>
2020-09-16 00:48:18 -0400

    added npm, eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
 };
-
-
 
 // created a component called App - returns a result of React.createElement
 // React components are something that returns a markup
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, {
-            name: "Luna",
-            animal: "Dog",
-            breed: "German Shepherd"
-        }),
-        React.createElement(Pet, {
-            name: "Zeus",
-            animal: "Dog",
-            breed: "Labrador"
-        }),
-        React.createElement(Pet, {
-            name: "Doinks",
-            animal: "Cat",
-            breed: "Mixed"
-        })
-    );
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "German Shepherd",
+    }),
+    React.createElement(Pet, {
+      name: "Zeus",
+      animal: "Dog",
+      breed: "Labrador",
+    }),
+    React.createElement(Pet, {
+      name: "Doinks",
+      animal: "Cat",
+      breed: "Mixed",
+    })
+  );
 };
 
 // anything in the div get overwritten by this render
 // here we are calling a composite component that we created above
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
