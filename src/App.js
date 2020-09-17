c24e0ddd3cb67f2741b4f1bff356fff230986072
Harsh Mehta <hsmehta1411@gmail.com>
2020-09-16 20:38:29 -0400

    async calls to Petfinder API, update Pet component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-// import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 
 // created a component called App - returns a result of React.createElement
 // React components are something that returns a markup
 const App = () => {
-  //   return React.createElement(
-  //     "div",
-  //     {},
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Luna",
-  //       animal: "Dog",
-  //       breed: "German Shepherd",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Zeus",
-  //       animal: "Dog",
-  //       breed: "Labrador",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Doinks",
-  //       animal: "Cat",
-  //       breed: "Mixed",
-  //     })
-  //   );
-
   return (
     <div>
       <h1>Adopt Me!</h1>
       <SearchParams />
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Zeus" animal="Dog" breed="GS" />
-      <Pet name="Doinks" animal="Cat" breed="Mixed" /> */}
     </div>
   );
 };
